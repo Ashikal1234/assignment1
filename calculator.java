364e90b29d51b932dfb8e18f6a9d8b964b8f4760
Ashikal1234 <101807268+Ashikal1234@users.noreply.github.com>
2022-03-22 15:33:14 +0530

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,28 @@
+public class calculator {
+  int a,b;
+  public int add(int a,int b)
+  {
+      int c=a+b;
+      return c;
+  }
+  
+public int sub(int a ,int b)
+{
+
+int c=a-b;
+return c;
+
+}
+public int multiply(int a ,int b)
+{
+
+int c=a*b;
+return c;
+}
+public float div(int a,int b)
+{
+    int c=a/b;
+    return c;
+}
+    
+}
